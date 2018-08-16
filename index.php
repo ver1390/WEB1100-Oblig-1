ecd81c4027da39aa3354c181f3c7a334b59aff69
msingstad <40832199+msingstad@users.noreply.github.com>
2018-08-16 23:05:58 +0200

    Hamburgermeny

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <title>Filosofi</title>
     </head>
     <body>
